5b22830de3ab07e48442f8627992b3138cd2cb12
Daan Verbeek <daanverbeek13@gmail.com>
2024-03-01 10:51:00 +0100

    opdracht 9.1.2

--- src/main.cpp
+++ src/main.cpp
@@ -8,82 +8,95 @@ const int PIN_BUTTON_1 = 8;   // The number of the button 1 (KEY1) pin.
 const int PIN_BUTTON_2 = 9;   // The number of the button 2 (KEY2) pin.
 int ledOrder[] = {PIN_LED_RED, PIN_LED_GREEN, PIN_LED_BLUE};
 int prevOrderIndex = 0;
-int currentOrderIndex = 0;
+int currentOrderIndex = 2;
 
 int buttonState_1 = 0; // Keep track of the button 1 state.
 int buttonState_2 = 0; // Keep track of the button 2 state.
 int count = 0;
-int leavingCount = 0;
 byte prevButtonOneState = HIGH;
 byte prevButtonTwoState = HIGH;
 unsigned long previousMillis = 0;
 const long interval = 100;  // interval at which to blink (milliseconds)
 
 void toggleLED() {
-    digitalWrite(ledOrder[prevOrderIndex], LOW);
-    digitalWrite(ledOrder[currentOrderIndex], HIGH);
-
+   
     prevOrderIndex = currentOrderIndex;
-
     if (currentOrderIndex == 2) {
-    currentOrderIndex = 0;
+        currentOrderIndex = 0;
     } else {
-      currentOrderIndex++;
+        currentOrderIndex++;
     } 
     delay(100);
-  
+    digitalWrite(ledOrder[prevOrderIndex], LOW);
+    digitalWrite(ledOrder[currentOrderIndex], HIGH);
 }
 
 void buttonEnterHandler(int BUTTON_PIN, int LED_PIN) {
-  unsigned long currentMillis = millis();
+    unsigned long currentMillis = millis();
 
-  const int buttonState = digitalRead(BUTTON_PIN);
-  previousMillis = currentMillis;
-  if (buttonState == prevButtonOneState) return;
+    const int buttonState = digitalRead(BUTTON_PIN);
+    previousMillis = currentMillis;
+    if (buttonState == prevButtonOneState) return;
 
-  if (buttonState == LOW) {
-    toggleLED();
-    count++;
-    Serial.println("Entering: "  + String(count));
-  } 
+    if (buttonState == LOW) {
+        toggleLED();
+        count++;
+        Serial.println("Total Festival Visitors: "  + String(count));
+    } 
 
-  prevButtonOneState = buttonState;
+    prevButtonOneState = buttonState;
 }
 
 void buttonLeaveHandler(int BUTTON_PIN, int LED_PIN) {
-  unsigned long currentMillis = millis();
+    unsigned long currentMillis = millis();
 
-  const int buttonState = digitalRead(BUTTON_PIN);
+    const int buttonState = digitalRead(BUTTON_PIN);
     previousMillis = currentMillis;
     if (buttonState == prevButtonTwoState) return;
 
     if (buttonState == LOW) {
-      digitalWrite(LED_PIN, HIGH);
-      leavingCount++;
-      delay(100);
-      Serial.println("Leaving: "  + String(leavingCount));
-      digitalWrite(LED_PIN, LOW);
-    } 
+        if (count <= 0) {
+          Serial.println("Visitor Count will go under 0. Not possible");
+          return;
+        }
+        count--;
+        delay(100);
+        Serial.println("Total Festival Visitors: "  + String(count));
+
+
+        if (currentOrderIndex == 0) {
+            prevOrderIndex = currentOrderIndex;
+            currentOrderIndex = 2;
+        } else {
+            prevOrderIndex = currentOrderIndex;
+            currentOrderIndex--;
+        }
+        
+        delay(100);
+        Serial.println("CurrentorderIndex: "  + String(currentOrderIndex));
+        Serial.println("prevOrderIndex: "  + String(prevOrderIndex));
+        digitalWrite(ledOrder[prevOrderIndex], LOW);
+        digitalWrite(ledOrder[currentOrderIndex], HIGH);
+    }
 
     prevButtonTwoState = buttonState;
 }
 
 void setup() {
-  Serial.begin(9600);
+    Serial.begin(9600);
 
-
-  pinMode(PIN_LED_RED, OUTPUT);
-  pinMode(PIN_LED_GREEN, OUTPUT);
-  pinMode(PIN_LED_BLUE, OUTPUT);
-  pinMode(PIN_BUTTON_1, INPUT_PULLUP);
-  pinMode(PIN_BUTTON_2, INPUT_PULLUP);
+    pinMode(PIN_LED_RED, OUTPUT);
+    pinMode(PIN_LED_GREEN, OUTPUT);
+    pinMode(PIN_LED_BLUE, OUTPUT);
+    pinMode(PIN_BUTTON_1, INPUT_PULLUP);
+    pinMode(PIN_BUTTON_2, INPUT_PULLUP);
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
+    unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {
-    buttonEnterHandler(PIN_BUTTON_1, PIN_LED_BLUE);
-    buttonLeaveHandler(PIN_BUTTON_2, PIN_LED_YELLOW);
-  }
+    if (currentMillis - previousMillis >= interval) {
+        buttonEnterHandler(PIN_BUTTON_1, PIN_LED_BLUE);
+        buttonLeaveHandler(PIN_BUTTON_2, PIN_LED_YELLOW);
+    }
 }
